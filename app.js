e4ba7bd14475857ebc6b261a6bcde52f2ad80c77
Nico <93469940+nizikai@users.noreply.github.com>
2024-11-13 06:04:33 +0700

    Added pointer styling

--- app.js
+++ app.js
@@ -1,16 +1,65 @@
-window.addEventListener('scroll', function () {
-    // Get all full-screen sections
-    const fullScreenSections = document.querySelectorAll('#full-screen');
-    const windowHeight = window.innerHeight;
+///MARK: - Cursor
 
-    fullScreenSections.forEach((section, index) => {
-        // Get the position of the section relative to the viewport
-        const sectionTop = section.getBoundingClientRect().top;
+// Cursor creation
+const cursor = document.createElement('div');
+cursor.classList.add('cursor');
+document.body.appendChild(cursor);
 
-        // Check if the section has entered the viewport
-        if (sectionTop <= windowHeight && sectionTop > 0) {
-            // Set the background image
-            section.style.backgroundImage = `url('Assets/background-${index + 1}.png')`; // Replace with your image paths
-        }
+// Update cursor position based on mouse coordinates
+function updateCursorPosition(e) {
+    //get the mouse position
+    const offsetX = e.clientX;
+    const offsetY = e.clientY;
+
+    //setting the cursor position
+    cursor.style.left = `${offsetX}px`;
+    cursor.style.top = `${offsetY}px`;
+}
+
+// Smoothly update cursor position on mouse move using requestAnimationFrame
+document.addEventListener('mousemove', (e) => {
+    requestAnimationFrame(() => updateCursorPosition(e));
+});
+
+// Shrink cursor on mouse down
+document.addEventListener('mousedown', () => {
+    cursor.classList.add('active');
+});
+
+// Restore cursor size on mouse up
+document.addEventListener('mouseup', () => {
+    cursor.classList.remove('active');
+});
+
+// Make cursor as a line while hovering on texts
+document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        cursor.classList.add('hover');
+    });
+    el.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hover');
     });
 });
+
+// Hide cursor while hovering links
+document.querySelectorAll('a').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        cursor.classList.add('hover-link');
+    });
+    el.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hover-link');
+    });
+});
+
+// Get the navigation menu icon to change its asset on hover
+const icon = document.getElementById('home-icon');
+
+icon.addEventListener('mouseenter', () => {
+    icon.src = './Assets/home-glow.png';
+});
+
+icon.addEventListener('mouseleave', () => {
+    icon.src = './Assets/home.png';
+});
+
+
